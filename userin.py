8a2b7f08c31722eb54f07378373d87e712c4e92c
Roopendra pratap kanam <roopendraprathapr58@gmail.com>
2026-04-15 11:38:34 +0530

    Add files via upload

---
+++ userin.py
@@ -0,0 +1,15 @@
+#taking user input using the input() method and split() function
+x = input("enter the name of the student:")
+print("roopendra is a good student",x)
+#taking the int type inputs
+y =int(input("enter the age of the student:"))
+print("age of the student is :",y)
+#using split() function
+x,y=int(input("enter the two values:")).split()
+print("number of boys:",x)
+print("number of girls:",y)
+#taking three inputs
+x,y,z=int(input("enter three values:")).split()
+print("total number of students:",x)
+print("total number of boys:",y)
+print("total number of girls:",z)
