8a84a5055e2c445252dc31f858f30b0107667edd
Roopendra pratap kanam <roopendraprathapr58@gmail.com>
2026-04-22 14:19:26 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,6 @@
+name = input("enter your name:")
+password = input("enter your pssword:")
+if name == "admin" and password == "1234":
+    print("login sucessful")
+else:
+    print("enter valid credentials")
