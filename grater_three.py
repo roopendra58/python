9dc9b236e707ebf4854ba688d14adcda44d5135a
Roopendra pratap kanam <roopendraprathapr58@gmail.com>
2026-04-22 13:40:16 +0530

    Add files via upload

---
+++ grater_three.py
@@ -0,0 +1,10 @@
+a=int(input("emter three numbers:"))
+b=int(input("emter three numbers:"))
+c=int(input("emter three numbers:"))
+
+if a>=b and a>=c:
+    print("a is larger:",a)
+elif b>=a and b>=c:
+    print("b is larger:",b)
+else:
+    print("c is larger:",c)
