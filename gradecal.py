88e098d82ea4341cc567f8e23a73676325c6f063
Roopendra pratap kanam <roopendraprathapr58@gmail.com>
2026-04-22 13:40:55 +0530

    Add files via upload

---
+++ gradecal.py
@@ -0,0 +1,11 @@
+m = int(input("enter your marks:"))
+if m >= 90:
+    print("first class A") 
+elif m >= 75:
+    print("second class B")
+elif m >= 50:
+    print("third class C")
+elif m >= 30:
+    print("fourth class D")
+else:
+    print("fail FHFHFHFHFHFHFHFFHFHFHFHFFHFHF")
